68e5ee298559d5a617c9389ec597629393e553cb
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-11-29 08:31:58 -0300

    Adicionando icones e desenvolvendo o componente rodape

--- Front-end/oldEngines/src/app/app.module.ts
+++ Front-end/oldEngines/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
