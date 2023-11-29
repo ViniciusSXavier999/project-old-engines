ae323aae2ec55b3d42f9e9386b0dae61e1b7f5f8
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-11-29 13:00:09 -0300

    Componente Entrar finalizado

--- Front-end/oldEngines/src/app/app.module.ts
+++ Front-end/oldEngines/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CadastroComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule
